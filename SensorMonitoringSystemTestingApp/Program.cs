8faebc93609900d24a97ca0daee6168bf937fb4a
AhmetOguzhanGunoz <oguzhangunoz@gmail.com>
2017-12-25 12:42:03 +0300

    Sensor Monitoring System Service and Client
    This project consists of a database, a web service which uses this constructed database and a client which uses this developed web service. Also, the clients pages are responsive. The graphical demonstration of sensor datas was generated by the help of Google Charts .js. This project is developed by me in my internship period.

---
+++ SensorMonitoringSystemTestingApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using SensorMonitoringSystemTestingApp.SensorMonitoringSystemServiceReference;
+
+namespace SensorMonitoringSystemTestingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SensorMonitoringSystemServiceClient client = new SensorMonitoringSystemServiceClient();
+            client.Open();
+            var allCompanies = client.FindAllCompanies();
+            Console.WriteLine(allCompanies.ToString());
+            client.Close();
+        }
+    }
+}
---
+++ SensorMonitoringSystemUnitTestProject/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SensorMonitoringSystemUnitTestProject.SensorMonitoringSystemServiceReference;
+
+namespace SensorMonitoringSystemUnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        SensorMonitoringSystemServiceClient SMSSE = new SensorMonitoringSystemServiceClient();
+
+        [TestMethod]
+        public void TestUpperCaseUsername()
+        {           
+            Assert.AreEqual(true, SMSSE.UsernameControl("OGUZHANGUNOZ"));
+        }
+
+        [TestMethod]
+        public void TestLowerCaseUsername()
+        {
+            Assert.AreEqual(true, SMSSE.UsernameControl("oguzhangunoz"));
+        }
+
+
+    }
+}
